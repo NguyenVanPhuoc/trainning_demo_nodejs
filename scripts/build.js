15c9954b88569e4fcbc86ef138c57fbe56761aa2
NguyenVanPhuoc <vanphuoc260797@gmail.com>
2024-08-16 16:51:45 +0700

    Update

--- scripts/build.js
+++ scripts/build.js
@@ -9,3 +9,10 @@ ncp(sourceDir, destDir, function (err) {
 	}
 	console.log('done!');
 });
+ncp('.env', 'dist/.env', function (err) {
+	if (err) {
+		return console.error(err);
+	}
+	console.log('done!');
+});
+
